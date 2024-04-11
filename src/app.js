87edcf034c6b5a18f61466571502d8d57faa09e2
greeneley <anhchangtoanhoc97@gmail.com>
2024-04-11 22:28:41 +0700

    Install helmet (hide security information) and compression (reduce the bandwidth)

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const morgan  = require('morgan');
+const helmet = require("helmet");
+const compression = require("compression");
 const app = express();
 
 // init middleware
 app.use(morgan("dev"))
+app.use(helmet());
+app.use(compression());
 
 // init database
 
 // init routes
+app.get('/', (req, res, next) => {
+    return res.status(500).json({
+        message: "welcome fantipjs"
+    })
+})
+
 
 // handle error
 
