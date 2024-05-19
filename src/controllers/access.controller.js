bca89354052ecdabac934e438484a162ee64ce7a
greeneley <anhchangtoanhoc97@gmail.com>
2024-05-19 11:04:47 +0700

    Implement SuccessHandler

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,10 +1,18 @@
 'use strict'
 
 const AccessService = require("../services/access.service");
+const {CREATED} = require("../core/success.response");
 
 class AccessController {
     signUp = async (req, res, next) => {
-        return res.status(201).json(await AccessService.signUp(req.body))
+        
+        new CREATED({
+            message: 'Registered OK!',
+            metadata: await AccessService.signUp(req.body),
+            options: {
+                limit: 12
+            }
+        }).send(res);
     }
 }
 
