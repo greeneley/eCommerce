78405ed7ece6cb7d59732b082c9e4921ba2b8849
greeneley <anhchangtoanhoc97@gmail.com>
2024-06-23 21:17:11 +0700

    Build product schema

---
+++ src/models/product.model.js
@@ -0,0 +1,82 @@
+"use strict";
+
+import { model, Schema } from "mongoose";
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+const productSchema = new Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_thumb: {
+      type: String,
+      required: true,
+    },
+    product_description: String,
+    product_price: {
+      type: Number,
+      required: true,
+    },
+    product_quantity: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ["Electronics", "Clothing", "Furniture"],
+    },
+    product_shop: String,
+    product_attributes: {
+      type: Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  },
+);
+
+// define the product type = clothing
+
+const clothingSchema = new Schema(
+  {
+    brand: {
+      type: String,
+      required: true,
+    },
+    size: String,
+    material: String,
+  },
+  {
+    collection: "clothes",
+    timestamps: true,
+  },
+);
+
+// define the product type = Electronics
+
+const electronicSchema = new Schema(
+  {
+    manufacturer: {
+      type: String,
+      required: true,
+    },
+    model: String,
+    color: String,
+  },
+  {
+    collection: "electronics",
+    timestamps: true,
+  },
+);
+
+module.exports = {
+  product: model(DOCUMENT_NAME, productSchema),
+  electronic: model("Electronics", electronicSchema),
+  clothing: model("Clothing", clothingSchema),
+};
--- src/services/access.service.js
+++ src/services/access.service.js
@@ -1,4 +1,4 @@
-``"use strict";
+"use strict";
 
 const shopModel = require("../models/shop.model");
 const bcrypt = require("bcryptjs");
